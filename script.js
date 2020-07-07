4f300b480ffee7b04184bfd7b7cbafb4fa42e9d6
Andy Gauthier <amgauthier1992@gmail.com>
2020-07-07 13:50:49 -0400

    finished prompt

--- script.js
+++ script.js
@@ -10,55 +10,69 @@
 
 //GET /users/:username/repos
 
+// Parameters
+// Name 	  Type 	   Description
+// type 	  string   Can be one of all, owner, member. Default: owner
+// sort 	  string   Can be one of created, updated, pushed, full_name. Default: full_name
+// direction  string   Can be one of asc or desc. Default: asc when using full_name, otherwise desc
+
 'use strict';
 
-// function formatQueryParams(params) {
-//     const queryItems = Object.keys(params)
-//         .map(key => `${encodeURIComponent(key)}=${encodeURIComponent(params[key])}`)
-//     return queryItems.join('&');
-// }
+function displayRepos(responseJson){
+    //responseJson is an array of objects
+    $('#results-list').empty();
+    $('#results').removeClass('hidden')
+    for (let i = 0; i < responseJson.length; i++){
+        $('#results-list').append(
+            `<li>
+            <h2>${responseJson[i].name}</h2>
+            <a href="${responseJson[i].html_url}">${responseJson[i].html_url}</a> 
+            </li>`   
+        )
+    }
+}
 
-// function displayResults(responseJson, maxResults){
-//     console.log(responseJson);
-//     $('#results-list').empty();
-//     for (let i = 0; i < responseJson.value.length && i < maxResults ; i++){
-//         $('#results-list').append(
-//             //repos go here
-//         )};
-//     $('results').removeClass('hidden');
-// };
-
-// function getRepos(query) {
-//     const params = {
-//         q: query,
-//         pageSize: maxResults
-//     };
-//     const queryString = formatQueryParams(params)
-//     const url = searchURL + '?' + queryString;
-
-//     const options = {
-//         headers: new Headers({
-
-//         })
+function getRepos(handle){
+//use backticks as we are dynamically adding handle to url
+const url = `https://api.github.com/users/${handle}/repos`
+console.log(url);
+// const options = {
+//     method: "GET",
+//     headers: {
+//         //typically API key is string. some are unique numbers. depends on API 
+//         api_key: "123456"
 //     }
-// };
-
-// fetch(url, options)
-//     .then(response => {
-//         if (response.ok) {
-//             return response.json();
-//         }
-//         throw new Error(response.statusText);
-//     })
-//     .then(responseJson => displayResults)
+//     //we can clarify info here about different headers and pass through to fetch
+// }
+// fetch(url,options)
+fetch(url)
+    .then(response => { 
+        if (response.ok) {
+            return response.json();
+        }
+        throw new Error(response.statusText);
+        })
+    .then(responseJson => displayRepos(responseJson))
+}
 
 function watchForm() {
 $('form').submit(function(event){
+    console.log(`watchForm ran`);
     event.preventDefault();
     const handle = $('#search-handle').val();
     console.log(handle);
+    //pass it through to function getRepos. In order to get the repos we need to
+    //parse the url. In order to parse the url we need the person's github handle.
     getRepos(handle)
     });
 }
 
-$(watchForm);
+//only have 1 event handle, dont need to make a larger master function. 
+watchForm();
+
+// first read through js
+// -event binded to Form
+// -when you click submit event is triggered
+
+// fetch-a method that is prepared to connect with `apis`
+// acts similar to postman
